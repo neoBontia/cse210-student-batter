c041bb4cc229b20a8d3bd105f536fe3632be3b54
neoBontia <bon21004@byui.edu>
2021-11-05 03:19:46 +0800

    Added handle_collisions_action.py

---
+++ batter/game/handle_collisions_action.py
@@ -0,0 +1,19 @@
+import random
+from game import constants
+from game.action import Action
+
+
+class HandleCollisionsAction(Action):
+    """A code template for handling collisions. The responsibility of this class of objects is to update the game state when actors collide.
+    
+    Stereotype:
+        Controller
+    """
+
+    def execute(self, cast):
+        """Executes the action using the given actors.
+
+        Args:
+            cast (dict): The game actors {key: tag, value: list}.
+        """
+        # TO DO CODE HERE
