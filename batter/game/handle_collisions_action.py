469315d8b653925947be1b7ddf23c9e35551e0da
neoBontia <bon21004@byui.edu>
2021-11-06 12:08:58 +0800

    Added bricks, paddle, and ball collision, and modified frame rate

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -1,6 +1,7 @@
 import random
 from game import constants
 from game.action import Action
+from game.point import Point
 
 
 class HandleCollisionsAction(Action):
@@ -16,4 +17,22 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
-        # TO DO CODE HERE
+        ball = cast["ball"][0]
+        paddle = cast["paddle"][0]
+        bricks = cast["brick"]
+
+        for offset in range(len(paddle.get_text())):
+            point = Point(paddle.get_position().get_x() + offset, paddle.get_position().get_y())
+            if ball.get_position().equals(point):
+                reverse = ball.get_velocity().reverse()
+                new_direction = Point(random.choice([1,-1]), reverse.get_y())
+                ball.set_velocity(new_direction)
+        
+        for brick in bricks:
+            if ball.get_position().equals(brick.get_position()):
+                bricks.remove(brick)
+                
+                reverse = ball.get_velocity().reverse()
+                new_direction = Point(random.choice([1, -1]), reverse.get_y())
+                ball.set_velocity(new_direction)
+        
